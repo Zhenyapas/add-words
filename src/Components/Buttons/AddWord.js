a13fcbc634fe21cafc24ce30f889d6367e32ec5b
Zhenyapas <zhekazhekapas@gmail.com>
2022-10-17 14:39:24 +0300

    React hook form with custom validation, checking for a word matching

--- src/Components/Buttons/AddWord.js
+++ src/Components/Buttons/AddWord.js
@@ -9,36 +9,147 @@ import { useForm } from 'react-hook-form';
 
 
 
+
+
 export default function AddWord(props) {
   
   
   const [open, setOpen] = useState(false);
+  
 
-   const {register,handleSubmit,formState:{errors,isValid},reset,setError} = useForm({
+ 
+
+  const index = useRef(false);
+  const index2 = useRef(false);
+  const button = useRef();
+
+  
+
+   const {register,handleSubmit,formState:{errors,isValid},reset,setError,clearErrors} = useForm({
      mode:'onChange',
  });
 
-  const [value, setValue] = useState('')
+
   console.log("RENDER INPUTS");
   const handleClickOpen = () => {
     setOpen(true);
   };
 
   const handleClose = (e) => {
-    console.log(errors);
     setOpen(false);
+    index.current = false;
     reset();
   };
-
-  console.log((Object.keys(errors).length));
 
   const onSubmit = data => {
     console.log(data);
     handleClose()
   };
 
+  const useTextFieldValidation = () => {
+
+   
+
+    let helperText,error,color;
+
+    return (inputName = '') => {
+
+
+    
+
+
+
+      if(errors[inputName]) {
+      
+      error = true;
+        switch (errors[inputName].type) {
+          case 'required': 
+            
+            helperText = inputName + ' :Your field is empty!';
+            break;
+          case 'fieldName' :
+            helperText = 'You had this word';
+
+            if(index) {
+            console.log('Works')
+            error = false;
+            color = 'warning';
+           
+
+            break;
+          }
+          case 'fieldTranslation':
+
+            helperText ='Add another translation of this word.';
+            if(index2.current === 'Error'){
+              error = true;
+              helperText=  'You had this translation!';
+            }
+      } 
+    }
+    
+
+
+      return  { helperText,error,color} ;
+    }
+
+  }
+
+  let textFieldValidation1 = useTextFieldValidation();
+  let textFieldValidation2 = useTextFieldValidation();
+  
+ 
+
+
+
+
+  console.log(index);
+
+
 
   
+  const checkNewWord = (e,inputName) => {
+
+    console.log(errors);
+   
+
+
+    switch (inputName) {
+ 
+      case 'name':
+
+        let indexError = props.words.findIndex((elem) => (elem.name.toLowerCase().trim() === e.target.value.toLowerCase()));
+        if(indexError !== -1) {
+
+          index.current = indexError;
+          console.log('you had this word');
+         
+        } else { 
+          index.current = false;
+        }
+
+        break;
+    
+      case 'translation':
+
+        let error = (index.current) ? props.words[index.current].translation.map((elem) => elem.toLowerCase().trim())
+        .includes(e.target.value.toLowerCase().trim()) : false; 
+
+        if(error){
+
+           console.log('You had this word translation');
+           index2.current='Error';
+      
+ 
+        } else { 
+          index2.current = false;
+         
+        }
+
+
+    }
+
+  }
 
   return (
     <div>
@@ -58,16 +169,29 @@ export default function AddWord(props) {
               autoFocus
               autoComplete='off'
               margin="dense"    
-              type="text"
               fullWidth
               variant="standard"
+              
+              {...textFieldValidation1('name')}
+              
+
               {...register('name', {
                 required:true,
-                maxLength:10,
-                onChange: (e) => console.log(e) 
+                hasTranslation:true,
+                onChange: (e) => {
+                  checkNewWord(e,'name');
+                },
+                validate : {
+                  fieldName : (word) => {
+                    if(index && !index2) return true;
+                    return !(props.words.map((e) => e.name.toLowerCase()).some((elem) => word === elem.toLowerCase().trim() )) 
+                  },
+                }
               })}
+              
              
             /> 
+            
             <TextField
                 autoFocus
                 label='Translation'
@@ -76,15 +200,30 @@ export default function AddWord(props) {
                 autoComplete='off'
                 fullWidth
                 variant="standard"
-               
-                {...register('translation',{ required: true })}
+                {...textFieldValidation2('translation')}
+
+                {...register('translation',{ 
+                  required: true,
+                  onChange: (e) => {
+                    checkNewWord(e,'translation');
+                  },
+                  validate : {
+                    fieldTranslation: (word) => {
+                      console.log(index2)
+                      if(index2.current === 'Error') {
+                       return !props.words[index.current].translation.map((elem) => elem.toLowerCase().trim())
+                        .includes(word.toLowerCase().trim()) 
+                      }
+                      return true
+                  }
+                 }})}
               />
  
         </DialogContent>
 
         <DialogActions sx={{backgroundColor:'background.paper',}}>
           <Button onClick={handleClose}  sx={{mb:1}}>Cancel</Button>
-          <Button id='button-add-word' type='submit' disabled={!isValid}  sx={{mr:1,mb:1}}>Add word</Button>
+          <Button id='button-add-word'  ref={button} type='submit' disabled={!isValid}  sx={{mr:1,mb:1}}>Add word</Button>
         </DialogActions>
 
        </form>
